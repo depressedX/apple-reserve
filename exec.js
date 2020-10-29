f403213a224e49853e5af2a299884fbee97fce8f
liupenghao03 <liupenghao03@kuaishou.com>
2020-10-29 10:43:14 +0800

    xxx

--- exec.js
+++
@@ -1,9 +0,0 @@
-const {exec} = require('./utils')
-
-const timer = setInterval(() => {
-    exec().then(res => {
-        if (res) {
-            clearInterval(timer)
-        }
-    }).catch(() => {})
-}, 1000)
--- findAny.js
+++
@@ -1,9 +0,0 @@
-const {findAnyPhoneAndOpen} = require('./utils')
-
-const timer = setInterval(() => {
-    findAnyPhoneAndOpen().then(res => {
-        if (res) {
-            clearInterval(timer)
-        }
-    })
-}, 1000)
